4c6bbfa2ae84bb63d58d847cd3637e25e14d5ce9
onoff562 <160796583+onoff562@users.noreply.github.com>
2024-03-01 01:02:18 +0700

    Add files via upload

---
+++ bin/main.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+var advance = require(lib + '/advance_math.js');
+
+module.exports = {
+	multiplication: advance.multiplication,
+	division: advance.division,
+};
